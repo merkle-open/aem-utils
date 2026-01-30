a34096da0f1473a26d11febdd4b6278460bac8b4
Michael Hörenbeg <michael.hoerenberg@merkle.com>
2026-01-30 13:48:07 +0100

    #NOISSUE debugging SonarQube test coverage

--- core/src/test/java/com/merkle/oss/aem/utils/java/ClassUtilTest.java
+++ core/src/test/java/com/merkle/oss/aem/utils/java/ClassUtilTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,13 +17,19 @@ class ClassUtilTest {
      * Method under test: {@link ClassUtil}
      */
     @Test
-    void instantiationException() throws NoSuchMethodException {
-        final Constructor<ClassUtil> constructor = ClassUtil.class.getDeclaredConstructor();
-        constructor.setAccessible(true);
+    void testAssertNoInstance() {
+        final AssertionError error = assertThrows(AssertionError.class, () -> ClassUtil.assertNoInstance(String.class));
+        assertTrue(error.getMessage().contains("String is not meant to be instantiated"));
+    }
 
-        final InvocationTargetException exception = assertThrows(InvocationTargetException.class, constructor::newInstance);
-        assertEquals(AssertionError.class, exception.getCause().getClass());
-        assertTrue(exception.getCause().getMessage().contains("not meant to be instantiated"));
+    @Test
+    void testConstructorIsPrivate() throws Exception {
+        final Constructor<ClassUtil> constructor = ClassUtil.class.getDeclaredConstructor();
+        assertTrue(Modifier.isPrivate(constructor.getModifiers()), "Constructor should be private");
+
+        constructor.setAccessible(true);
+        final InvocationTargetException ite = assertThrows(InvocationTargetException.class, constructor::newInstance);
+        assertInstanceOf(AssertionError.class, ite.getCause());
     }
 
 }
